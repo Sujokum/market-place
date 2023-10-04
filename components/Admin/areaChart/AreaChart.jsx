b7e19b55b4dea429d1a5fe7b41455f1e56bf4782
Sujokum <suneelahmed413@gmail.com>
2023-10-05 04:19:27 +0500

    addChanges

--- components/Admin/areaChart/AreaChart.jsx
+++ components/Admin/areaChart/AreaChart.jsx
@@ -1,5 +1,5 @@
 import React , {useEffect , useRef} from 'react'
-import Chart from 'chart.js/auto';
+import Chart from 'chart.js';
 
 const AreaChart = ({data}) => {
     const chartRef = useRef(null);
--- components/Admin/pieChart/PieChart.jsx
+++ components/Admin/pieChart/PieChart.jsx
@@ -1,6 +1,6 @@
 
 import React , {useEffect , useRef} from 'react'
-import Chart from 'chart.js/auto';
+import Chart from 'chart.js';
 const PieChart = ({data}) => {
     const chartRef = useRef(null);
 const chartInstance = useRef(null);
